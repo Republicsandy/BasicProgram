c04b7c8608fe204febd6fd39b0aee78a1f0711c3
Republicsandy <sandy180388@gmail.com>
2022-08-13 10:48:45 +0530

    [sandeep] FilipCoin file Added in UC_1_FilpCin

---
+++ BasicProgram/FlipCoin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    class FlipCoinPercentage
+    {
+        int Head = 0;
+        int Tail = 0;
+        public void FlipCoin()
+        {
+            Console.WriteLine("Enter the number of time you want to flip the coin :");
+            int NumberOfFilps = Convert.ToInt32(Console.ReadLine());
+            while (NumberOfFilps <= 0)
+            {
+                Console.WriteLine("Invalide input Enter the number of time you want to flip the coin");
+                NumberOfFilps = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < NumberOfFilps; i++)
+            {
+                Random flip = new Random();
+                int HoT = flip.Next(0, 2);
+                if (HoT < 0.5)
+                {
+                    Head++;
+                }
+                else
+                {
+                    Tail++;
+                }
+                float Head_Percentage = Head * 100 / NumberOfFilps;
+                float Tali_Percentage = Tail * 100 / NumberOfFilps;
+                Console.WriteLine("Percentage of Heads are:" + Head_Percentage);
+                Console.WriteLine("Percentage of Heads are:" + Tali_Percentage);
+            }
+        }
+    }
+}
---
+++ BasicProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("select the number for relative program");
+            Console.WriteLine(" 1 :for Flip Coin Percentage");
+
+            int Answer = Convert.ToInt32(Console.ReadLine());
+
+            switch (Answer)
+            {
+                case 1:
+                    FlipCoinPercentage flipcoin = new FlipCoinPercentage();
+                    flipcoin.FlipCoin();
+                    break;
+            }
+        }
+    }
+}
