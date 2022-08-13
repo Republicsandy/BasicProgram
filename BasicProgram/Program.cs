dd444629f7c8e0d980805232cca789dd3830a17f
Republicsandy <sandy180388@gmail.com>
2022-08-13 11:16:55 +0530

    [sandeep] Harmonic Number file Added in UC_4_HarmonicNumber

--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -11,6 +11,7 @@ namespace BasicProgram
             Console.WriteLine(" 1 :for Flip Coin Percentage");
             Console.WriteLine(" 2 :for LeapYear");
             Console.WriteLine(" 2 :for Power Of Two");
+            Console.WriteLine(" 3 :for Harmonic Number");
 
             int Answer = Convert.ToInt32(Console.ReadLine());
 
@@ -27,6 +28,10 @@ namespace BasicProgram
                 case 3:
                     PowerOfTwo poweroftwo = new PowerOfTwo();
                     poweroftwo.PowerOfTwoNumber();
+                    break;
+                case 4:
+                    Harmonic harmonicnumber = new Harmonic();
+                    harmonicnumber.HarmonicFunction();
                     break;
             }
         }
